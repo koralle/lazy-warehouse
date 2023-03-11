796e0af0bfd3da13148388d68f1ad7633ed9c206
koralle <mkhd7930@gmail.com>
2023-03-11 17:43:07 +0900

    fix

---
+++ backend/mux/mux.go
@@ -0,0 +1,39 @@
+package mux
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi/v5"
+
+	"github.com/koralle/lazy-warehouse/backend/config"
+	"github.com/koralle/lazy-warehouse/backend/graph"
+)
+
+func NewMux(config *config.Config) (http.Handler, func(), error) {
+	mux := chi.NewRouter()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
+	})
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.POST{})
+
+	mux.Post("/query", srv.ServeHTTP)
+
+	if config.Env == "dev" {
+		srv.Use(extension.Introspection{})
+		mux.Get("/", playground.Handler("GraphQL playground", "/query"))
+	}
+
+	return mux, func() { fmt.Println("Clean up.") }, nil
+
+}
