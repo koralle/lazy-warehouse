cdb52ebc11b43b82f13067328a14a6e9fc105cd7
koralle <mkhd7930@gmail.com>
2023-03-13 21:24:00 +0900

    add: TABLE `administration`

---
+++ backend/dockerfiles/postgresql/script/05_administration.sql
@@ -0,0 +1,13 @@
+\c lazy_warehouse;
+
+CREATE TABLE lazy_warehouse.administration (
+    id VARCHAR(255),
+    user_id VARCHAR(255) REFERENCES lazy_warehouse.users(id) NOT NULL,
+    group_id VARCHAR(255) REFERENCES lazy_warehouse.groups(id) NOT NULL,
+    role_id VARCHAR(255) REFERENCES lazy_warehouse.roles(id) NOT NULL,
+    _created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    _updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    CONSTRAINT administration_unit UNIQUE NULLS NOT DISTINCT (user_id, group_id, role_id) 
+);
+
